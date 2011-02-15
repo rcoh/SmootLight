84d914b0a015ef6c588253ac7f70382bfa9030e5
rcoh <rcoh@mit.edu>
2011-02-15 15:15:43 -0500

    Added documentation to repo in docs folder.

---
+++ layouts/SpecifiedLayout.py
@@ -0,0 +1,21 @@
+from operationscore.PixelAssembler import *
+class SpecifiedLayout(PixelAssembler):
+    """SpecifiedLayout is a class that allows precise specification of each individual LED.
+    Configure with a <Locations> tag in the args dict as follows':
+    <Args>
+        <Locations>
+            <Loc>(1,1)</Loc>
+            <Loc>(50,50)</Loc>
+        </Locations>
+        etc.
+    </Args>
+    You may put attributes on the Locs so that you don't get confused.
+    """
+
+    def layoutInit(self):
+        self.lightNum = -1
+
+    def layoutFunc(self, lastLocation):
+        self.lightNum += 1
+        return self['Locations'][self.lightNum]
+
