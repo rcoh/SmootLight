b5d2ab8a6f486c97fcbade14498e7f3e94f01199
dlaw <dlaw@mit.edu>
2011-02-18 12:25:48 -0500

    made PixelAssembler more pythonic

--- operationscore/PixelAssembler.py
+++ operationscore/PixelAssembler.py
@@ -5,24 +5,20 @@ class PixelAssembler(SmootCoreObject):
     def init(self):
         self.validateArgs('PixelAssembler.params')
         self.initLayout()
-    def layoutFunc(self, lastLocation): #Must be defined by inheriting class.
-        #Returns tuple pair (x,y)
-        pass
+    def layoutFunc(self, lastLocation):
+        raise NotImplementedError
     def getPixelLocations(self): #returns a complete list of locations of Pixels
         #for a strip
         locations = [self.argDict['originLocation']]
         for pixelIndex in range(self['numPixels']-1): #-1 because origin
             #already exists
             newLocation = self.layoutFunc(locations[-1]) 
-            if newLocation == None:
-                raise Exception('Location cannot be null.  layoutFunc not \
-                defined or improperly defined.')
-            if Geo.dist(newLocation, locations[-1]) > \
-                    self['pixelToPixelSpacing']:
-                        raise Exception('Illegal pixel location.  Distance \
-                        between adjacent pixels must be less than \
-                        pixelToPixelSpacing.  Illegal distance is between '+str(pixelIndex) + ' and'\
-                                         + str(pixelIndex+1))
+            if (Geo.dist(newLocation, locations[-1]) >
+                self['pixelToPixelSpacing']):
+                raise Exception('Illegal pixel location.  Distance between adjacent ' 
+                                + 'pixels must be less than pixelToPixelSpacing.'
+                                + 'Illegal distance is between {0} and {1}'
+                                .format(pixelIndex, pixelIndex+1))
             locations.append(newLocation)
         if self['Reverse']:
             locations.reverse()
