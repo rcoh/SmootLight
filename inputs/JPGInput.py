8641b22a5dc3f092a8746658b52de321b8c8b150
rcoh <rcoh@mit.edu>
2011-02-25 15:49:37 -0500

    changing behavior of JPGInput.  Adding ImageToPixels to render images efficiently.

--- inputs/JPGInput.py
+++ inputs/JPGInput.py
@@ -1,32 +1,20 @@
 from operationscore.Input import *
 from PIL import Image
-import util.ComponentRegistry as compReg
 import os
 class JPGInput(Input):
     def inputInit(self):
         self.images = []
         self.imageIndex = 0
-        compReg.getLock().acquire()
-        minX,minY,maxX,maxY = compReg.getComponent('Screen').getSize()
-        compReg.getLock().release()
-        sWidth = maxX-minX
-        sHeight = maxY-minY
-        for filename in os.listdir(self['Directory']):
+        files = os.listdir(self['Directory'])
+        files.sort()
+        for filename in files:
+            print filename
             path = os.path.join(self['Directory'], filename)
             if '.jpg' in path or '.bmp' in path:
                 im = Image.open(path) #file to open
-                (w,h)=im.size
-                print w,h
-                xScale = sWidth/float(w)
-                yScale = sHeight/float(h)
-                pixels = []
-                for x in range(0,w,5):
-                   for y in range(0,h,5):
-                       rgb = im.getpixel((x,y))
-                       pixels.append({'Location':(int(x*xScale+minX),int(minY+y*yScale)),'Color':rgb})
-                self.images.append(pixels)
+                self.images.append(im)
 
     def sensingLoop(self):
         self.imageIndex += 1
         self.imageIndex = self.imageIndex % len(self.images)
-        self.respond(self.images[self.imageIndex])
+        self.respond(dict(Image=self.images[self.imageIndex]))
