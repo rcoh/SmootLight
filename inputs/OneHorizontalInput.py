df7f98dfecd9c5f86ecd522e5d9e8be906344cd7
Andrew Chen <moorheadandrew+github@gmail.com>
2011-02-28 19:04:06 -0500

    OneHorizontalInput now passes inputs as a list to fix input skips

--- inputs/OneHorizontalInput.py
+++ inputs/OneHorizontalInput.py
@@ -5,23 +5,15 @@ import util.Strings as Strings
 from operationscore.Input import *
 class OneHorizontalInput(Input):
     def inputInit(self):
-	self.barCount = 0
         compReg.getLock().acquire()
         self.minX, self.minY, self.maxX, self.maxY = compReg.getComponent('Screen').getSize()
         compReg.getLock().release()
     def sensingLoop(self):
-	time.sleep(0.02)
-        self.respond({Strings.LOCATION: (self.barCount,0)})
-	self.barCount += 1
-    def run(self):
-	time.sleep(0.1)
-        while 1:
-            try:
-                die = self.parentAlive()
-            except:
-                break
-	    while self.barCount < self.maxX/3:
-	        self.acquireLock()
-	        self.sensingLoop()
-        	self.releaseLock()
- 
+	time.sleep(0.5)
+	if not self.done:
+	    print "SDF"
+ 	    response = []
+ 	    for i in xrange(self.maxX):
+		response.append({Strings.LOCATION: (i,0)})
+	    self.respond(response)
+	    self.done = True
