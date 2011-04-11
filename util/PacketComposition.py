26a96e9fd181ed8571f210eeb37ab9fa56be06fd
dlaw <dlaw@mit.edu>
2011-04-11 12:25:15 -0400

    got rid of useless loop

--- util/PacketComposition.py
+++ util/PacketComposition.py
@@ -6,7 +6,6 @@ xmit = zeros(174, dtype='ubyte')
 xmit[:24] = bytearray('\x04\x01\xdcJ\x01\x00\x08\x01\x00\x00\x00\x00\x00\x00\x00\x00\x02\x00\x00\x00\x96\x00\xff\x0f')
 
 def composePixelStripPacket(pixelStrip, port, currentTime):
-    for i in range(30):
-        xmit[16] = port
-        xmit[24:] = concatenate([p.state(currentTime) for p in pixelStrip])
-        return xmit
+    xmit[16] = port
+    xmit[24:] = concatenate([p.state(currentTime) for p in pixelStrip])
+    return xmit
