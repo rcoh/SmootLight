2585a0f237bea97d92212fa6b96a11ee7836be67
Daniel Taub <dmtaub@gmail.com>
2011-04-28 20:13:55 -0400

    continuing

--- pixelevents/SynchTestEvent.py
+++ pixelevents/SynchTestEvent.py
@@ -1,10 +1,12 @@
 from operationscore.PixelEvent import *
-from numpy import array
+from numpy import array,zeros
 
 class SynchTestEvent(PixelEvent):
     """SynchTestEvent is an event to test the synchronization of the power supplies"""
     def initEvent(self):
         self.i = 0
     def state(self, time):
-        self.i = (self.i + 1) % 2
-        return array([self.i,0.,0.]), time+1
+        self.i = (self.i + 1) % 3
+        ret = zeros(3)
+        ret[self.i] = 1
+        return ret, time+1
