f7ad506969eedb1c85eb5a2959bfdd3640098cdf
dlaw <dlaw@mit.edu>
2011-04-30 16:51:57 -0400

    New event

---
+++ pixelevents/RiseFallEvent.py
@@ -0,0 +1,15 @@
+from operationscore.PixelEvent import *
+from numpy import array, exp
+
+class RiseFallEvent(PixelEvent):
+    """<Duration> -- length of time between first and last light."""
+    def state(self, time):
+        d = self.Duration
+        if time == 0:
+            print('returning {0}'.format([0, 4./d, -4./d/d]))
+            return [0, 4./d, -4./d/d], d
+        return None
+    
+    @staticmethod
+    def generate(duration):
+        return RiseFallEvent({'Duration': duration})
