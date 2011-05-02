010d1937e95996945d049ed9be20be5c57b77e8c
dxiao <dxiao@mit.edu>
2011-05-02 17:39:32 -0400

    Changes to RoughGum to get rid of piling inputs

--- behaviors/PedTrack.py
+++ behaviors/PedTrack.py
@@ -10,7 +10,7 @@ class PedTrack(Behavior):
             maxIntensity = self['MaxIntensity']
         else:
             maxIntensity = 10
- 
+        outputDict = {} 
         for sensory in sensor:
             opsensory = dict(sensory)
             if "SensorId" in opsensory:
@@ -20,6 +20,17 @@ class PedTrack(Behavior):
                 else:
                     opsensory["Location"] = {opsensory["Location"].keys()[0]:max(opsensory["Location"].values()[0] - 1, 0)}
             ret.append(opsensory)
-
-        return (ret, []) 
+        print ret
+        locDict = {}
+        for r in ret:
+            if 'Location' in r:
+                locDict = r['Location']
+                for key in locDict:
+                    locDict[key]=locDict[key]
+        if ret:
+            outputDict['Color'] = ret[0]['Color']
+            outputDict['Location'] = locDict
+            return ([outputDict], []) 
+        else:
+            return ([],[])
 
