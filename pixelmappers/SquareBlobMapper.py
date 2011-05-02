47e0cbe6b9b50f26b68800317db63d82e0b6d24c
Daniel Taub <dmtaub@gmail.com>
2011-05-02 13:59:16 -0400

    manual merge of parts of numpy

--- pixelmappers/SquareBlobMapper.py
+++ pixelmappers/SquareBlobMapper.py
@@ -1,10 +1,10 @@
 from operationscore.PixelMapper import *
 import numpy
-import util.ComponentRegistry as compReg
 
 # turn on all pixels within a square of side length 2*dist centered at
 # loc (so edges are orthogonal)
 
 class SquareBlobMapper(PixelMapper):
     def mappingFunction(self, inp, screen):
-        return numpy.amax(numpy.abs(screen.locs - inp[0]), -1) < inp[1]
+        # inp is a tuple (location, distance)
+        return numpy.amax(abs(screen.locs - inp[0]), -1) < inp[1]
