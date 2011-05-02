f9e917fa17a963aac479298bba5576728658477d
Daniel Taub <dmtaub@gmail.com>
2011-05-02 14:09:52 -0400

    fixed response mover

--- behaviors/ResponseMover.py
+++ behaviors/ResponseMover.py
@@ -1,14 +1,13 @@
 import pdb
 from operationscore.Behavior import *
 import util.ComponentRegistry as compReg
-class Mover(Behavior):
+class ResponseMover(Behavior):
     """ResponseMover is a scaffold for behaviors that spawn 'walkers' which act autonomously on input.
     To control the movment, use the behavior as part of a BehaviorChain and add a recursive hook which
     modulates the location."""
 
     def processResponse(self, sensorInputs, recursiveInputs):
         a = (recursiveInputs, recursiveInputs+self.addUID(sensorInputs))
-        print 'balls running:', len(recursiveInputs)
         return a
     def addUID(self, inputs):
         for i in inputs:
