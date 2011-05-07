cdf816152b5c3842f57b7c928b4160af8ee85dad
Russell Cohen <rcoh@mit.edu>
2011-05-06 23:34:09 -0400

    Fixed the layout, among some other things.

--- tools/UdpListener.py
+++ tools/UdpListener.py
@@ -1,11 +1,23 @@
 import socket
-
+import time
 HOST = ''
 PORT = 3344
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((HOST, PORT))
 
 (data,address) = sock.recvfrom(1024)
+lastt = time.time()
+c=0
+addresses = set()
 while data:
-    print data, ' @', address
+    c+=1
+    dt =( time.time()-lastt )
+    if dt > 1:
+	print c/dt, " packets per second from", len(addresses), "unique addresses"
+        c = 0
+        addresses = set()
+        lastt = time.time()
+    if 1:#address[-1]=='7':
+        print list(map(ord,data)), '\t', address
     (data, address) = sock.recvfrom(1024)
+    addresses.add(address)
